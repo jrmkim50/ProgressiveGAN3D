1b2451e5f4cc8eaeaeb62dcf3b8b6693b85ca37a
jrmkim50 <jrmkim50@gmail.com>
2022-01-31 17:47:36 -0800

    undoing previous change

--- dataset_3D.py
+++ dataset_3D.py
@@ -136,8 +136,7 @@ class TFRecordDataset:
         lod = int(np.floor(lod))
         assert minibatch_size >= 1 and lod in self._tf_datasets
         if self._cur_minibatch != minibatch_size or self._cur_lod != lod:
-            tfutil_3D.run(self._tf_init_ops[lod], feed_dict={self._tf_minibatch_in: minibatch_size})
-            # self._tf_init_ops[lod].run({self._tf_minibatch_in: minibatch_size})
+            self._tf_init_ops[lod].run({self._tf_minibatch_in: minibatch_size})
             self._cur_minibatch = minibatch_size
             self._cur_lod = lod
 
